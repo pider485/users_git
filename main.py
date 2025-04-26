e1c841eb4817d41a5865031107be6373f22227a3
pider485 <oleksijzolotarov4@gmail.com>
2025-04-26 12:22:16 +0300

    da

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Book(BaseModel):
     title: str = Field(..., min_lenght=3, max_lenght=255)
     author: str = Field(..., min_lenght=3, max_lenght=255)
     pages: int = Field(..., g=10)
+    da:str
 
 @app.get("/")
 async def get_all_books():
